ef6eb80192988a5cf09060a266afa3b6bbbbec06
sbr66 <bonam22@gmail.com>
2023-02-01 12:51:33 +0900

    magazine page 상품 카테고리별 추출 작업중

--- get_products.php
+++ get_products.php
@@ -1,16 +1,25 @@
 <?php
    include_once $_SERVER['DOCUMENT_ROOT'].'/magazine_b_back/dbconn.php';
-   include_once $_SERVER['DOCUMENT_ROOT'].'/magazine_b_back/error.php';
+//    include_once $_SERVER['DOCUMENT_ROOT'].'/magazine_b_back/error.php';
    $get_cate=$_GET['cate'];
+   $get_page=$_GET['page'];
 
    if(!isset($get_cate)){
-           if(isset($_GET['page'])){
-            printpage($_GET['page']);
+        if(isset($get_page)){
+            printpage($conn, $get_page);
         } else {
-            printpage(1);
+            printpage($conn, 1);
         }
+
    } else {
-    print_cate($conn); 
+        if(isset($get_page)){
+            print_cate($conn, $get_page, $get_cate); 
+            // echo "get page 있음";
+        } else {
+            print_cate($conn, 1, $get_cate);
+            // echo "get page 없음";
+        }
+
    }
     
     // if($_GET['cate'] == null){
@@ -23,35 +32,55 @@
         
     // }
 
-    function printpage(int $page){
-        // include_once $_SERVER['DOCUMENT_ROOT'].'/magazine_b_back/dbconn.php';
+    function printpage($conn, $page){
+        $page_info = total_page_num($conn);
 
-        // $start_data = ($page - 1)*10;
-        // $sql = "SELECT * FROM magb_magazine ORDER BY mag_idx DESC LIMIT $start_data, 10";
-        // $result = mysqli_query($conn, $sql);
+        $start_data = ($page - 1)*10;
+        $sql = "SELECT * FROM magb_magazine ORDER BY mag_idx DESC LIMIT $start_data, 10";
+        $result = mysqli_query($conn, $sql);
     
-        // $json_result = array(); 
-        // while($row = mysqli_fetch_array($result)){
-        //     array_push($json_result, array("mag_idx" => $row['mag_idx'], "mag_cate" => $row['mag_cate'], "mag_img1" => $row['mag_img1'], "mag_img2" => $row['mag_img2'], "mag_title" => $row['mag_title'], "mag_issue" => $row['mag_issue'], "mag_price" => $row['mag_price'],  "mag_desc" => $row['mag_desc'],  "mag_date" => $row['mag_date'])); // 첫번째 파라미터: 대상 배열, 두번째 파라미터는 배열 입력값
-        // }
-        // echo json_encode($json_result);
+        $json_result = array(); 
+        while($row = mysqli_fetch_array($result)){
+            array_push($json_result, array("mag_idx" => $row['mag_idx'], "mag_cate" => $row['mag_cate'], "mag_img1" => $row['mag_img1'], "mag_img2" => $row['mag_img2'], "mag_title" => $row['mag_title'], "mag_issue" => $row['mag_issue'], "mag_price" => $row['mag_price'],  "mag_desc" => $row['mag_desc'],  "mag_date" => $row['mag_date'], "page_info"=>$page_info)); // 첫번째 파라미터: 대상 배열, 두번째 파라미터는 배열 입력값
+        }
+        echo json_encode($json_result);
         
     }
 
-    function print_cate($conn){
-
+    function print_cate($conn, $page, $get_cate){
+            
+        $page_info = cate_page_num($conn, $get_cate);
         $get_cate=$_GET['cate'];
-
-        $sql = "SELECT * FROM magb_magazine WHERE mag_cate = '$get_cate'"; 
+        $start_data = ($page - 1)*10;
+        $sql = "SELECT * FROM magb_magazine WHERE mag_cate = '$get_cate' ORDER BY mag_idx DESC LIMIT $start_data, 10"; 
         $result = mysqli_query($conn, $sql); 
 
         $json_result = array(); 
         while($row = mysqli_fetch_array($result)){
-            array_push($json_result, array("mag_idx" => $row['mag_idx'], "mag_cate" => $row['mag_cate'], "mag_img1" => $row['mag_img1'], "mag_img2" => $row['mag_img2'], "mag_title" => $row['mag_title'], "mag_issue" => $row['mag_issue'], "mag_price" => $row['mag_price'],  "mag_desc" => $row['mag_desc'],  "mag_date" => $row['mag_date'], "cate"=>$get_cate)); // 첫번째 파라미터: 대상 배열, 두번째 파라미터는 배열 입력값
+            array_push($json_result, array("mag_idx" => $row['mag_idx'], "mag_cate" => $row['mag_cate'], "mag_img1" => $row['mag_img1'], "mag_img2" => $row['mag_img2'], "mag_title" => $row['mag_title'], "mag_issue" => $row['mag_issue'], "mag_price" => $row['mag_price'],  "mag_desc" => $row['mag_desc'],  "mag_date" => $row['mag_date'], "page_info" => $page_info)); // 첫번째 파라미터: 대상 배열, 두번째 파라미터는 배열 입력값
         }
         echo json_encode($json_result);
         // echo json_encode(array("cate" => 'abc'));
     }
    
+    function total_page_num($conn){
+        $sql = "SELECT * FROM magb_magazine ORDER BY mag_idx DESC"; 
+        $result = mysqli_query($conn, $sql); 
+    
+        $data_num =  mysqli_num_rows($result);
+        $page_num = ceil($data_num / 10);
+    
+        echo json_encode(array("data_num"=>$data_num, "page_num"=>$page_num));
+    }
+    
+    function cate_page_num($conn, $get_cate){
+        $sql = "SELECT * FROM magb_magazine  WHERE mag_cate = '$get_cate' ORDER BY mag_idx DESC"; 
+        $result = mysqli_query($conn, $sql); 
+    
+        $data_num =  mysqli_num_rows($result);
+        $page_num = ceil($data_num / 10);
+    
+        echo json_encode(array("data_num"=>$data_num, "page_num"=>$page_num));
+    }
 
 ?>
--- product_page.php
+++ product_page.php
@@ -2,12 +2,30 @@
 
 include_once $_SERVER['DOCUMENT_ROOT'].'/magazine_b_back/dbconn.php';
 
-$sql = "SELECT * FROM magb_magazine ORDER BY mag_idx DESC"; 
-$result = mysqli_query($conn, $sql); 
+$get_cate=$_GET['cate'];
+if(!isset($get_cate)){
+    total_page_num($conn);
+} else {
+    cate_page_num($conn, $get_cate);
+}
 
-$data_num =  mysqli_num_rows($result);
-$page_num = ceil($data_num / 10);
+function total_page_num($conn){
+    $sql = "SELECT * FROM magb_magazine ORDER BY mag_idx DESC"; 
+    $result = mysqli_query($conn, $sql); 
 
-echo json_encode(array("data_num"=>$data_num, "page_num"=>$page_num));
+    $data_num =  mysqli_num_rows($result);
+    $page_num = ceil($data_num / 10);
 
+    echo json_encode(array("data_num"=>$data_num, "page_num"=>$page_num));
+}
+
+function cate_page_num($conn, $get_cate){
+    $sql = "SELECT * FROM magb_magazine  WHERE mag_cate = '$get_cate' ORDER BY mag_idx DESC"; 
+    $result = mysqli_query($conn, $sql); 
+
+    $data_num =  mysqli_num_rows($result);
+    $page_num = ceil($data_num / 10);
+
+    echo json_encode(array("data_num"=>$data_num, "page_num"=>$page_num));
+}
 ?>
